3a5b26648571a73943bff4ee565a0ab215310ecb
FVI <test@examle.com>
2017-05-20 12:53:07 -0400

    my react starter

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin=require('html-webpack-plugin');
 
 
 module.exports={
-    entry:'./app/index.js';
+    entry:'./app/index.js',
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'index_bundle.js'
